ce6bbae2ee20ba7fd468c6c768b9e86ef2ab885a
dsv61999 <yuuta38240122@gmail.com>
2021-01-20 04:03:28 +0900

    second commit

--- numeron/go.mod
+++ numeron/go.mod
@@ -6,5 +6,5 @@ replace numeron/pkg => ./pkg
 
 require (
 	github.com/360EntSecGroup-Skylar/excelize/v2 v2.3.2
-	numeron/pkg v0.0.0-00010101000000-000000000000 // indirect
+	numeron/pkg v0.0.0-00010101000000-000000000000
 )
--- numeron/main.go
+++ numeron/main.go
@@ -7,5 +7,25 @@ import (
 )
 
 func main()  {
-	pkg.Hello()
+	pkg.DisplayCaption("NumeronApp", 2)
+	num_digits := pkg.InputDigitNum()
+
+	var computer pkg.Computer
+	computer.SetComputer(num_digits)
+	var player pkg.Player
+	player.InputNum(computer.Name, num_digits)
+
+	// computer := pkg.SetComputer(num_digits)
+	// f := excelize.NewFile()
+	// // Create a new sheet.
+	// index := f.NewSheet("Sheet2")
+	// // Set value of a cell.
+	// f.SetCellValue("Sheet2", "A2", "Hello world.")
+	// f.SetCellValue("Sheet1", "B2", 100)
+	// // Set active sheet of the workbook.
+	// f.SetActiveSheet(index)
+	// // Save spreadsheet by the given path.
+	// if err := f.SaveAs("Book1.xlsx"); err != nil {
+	// 	fmt.Println(err)
+	// }
 }
